ab49017fc309652667618c956904de5f034015dc
Anzhelika Kirichenko <evgenkir2015@gmail.com>
2026-04-17 15:36:39 +0300

    new configs and stuff

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,5 @@
 from src.model.baseline_model import BaselineModel
+from src.model.conformer import Conformer
 from src.model.lstm import LSTM
 
-__all__ = ["BaselineModel", "LSTM"]
+__all__ = ["BaselineModel", "LSTM", "Conformer"]
